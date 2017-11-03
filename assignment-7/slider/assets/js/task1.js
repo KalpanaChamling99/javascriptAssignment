43a25ff21b68bfa3ee16769a9dcae0840b5f6369
KalpanaChamling99 <Kalpanachamling99@gmail.com>
2017-11-03 12:16:31 +0545

    Digital Clock

--- assignment-7/slider/assets/js/task1.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  setInterval(slideImages,3000);
-  function slideImages(){
-    $('#images img:first-child').clone().appendTo('#images');
-    $('#images img:first-child').remove();
-  }
-}); 
